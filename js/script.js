f882eb2698e5f7518c327830cf9c5d6e0fa38c7a
Grapestheme <grapestheme@gmail.com>
2015-07-25 14:24:27 +0530

    Boost

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"user static";
+(function($) {
+
+    /*==============================================================*/
+    //WOW Animation  - START CODE
+    /*==============================================================*/
+
+    var wow = new WOW({
+        boxClass: 'wow',
+        animateClass: 'animated',
+        offset: 90,
+        mobile: false,
+        live: true
+    });
+    wow.init();
+
+})(jQuery);
